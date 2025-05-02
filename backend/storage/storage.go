fcab4f91f483e808dec1657eb41d52b171888a6a
Rustem Sharipov <sharipov.rustem@gmail.com>
2025-05-02 12:16:28 -0500

    The backend API is now Dockerized

--- backend/storage/storage.go
+++ backend/storage/storage.go
@@ -1,7 +1,9 @@
 package storage
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/sharipovr/pflDockerBilling/models"
 
@@ -12,7 +14,14 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	dsn := "host=localhost user=rustemsharipov password=postgres dbname=pfl_docker_billing port=5432 sslmode=disable"
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Ошибка подключения к БД: ", err)
