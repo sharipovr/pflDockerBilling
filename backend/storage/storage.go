28a5d5e155b0bdc50adf7a25282e172e314700ca
Rustem Sharipov <sharipov.rustem@gmail.com>
2025-05-04 21:37:12 -0500

    Dockerized all parts of application (working with Stripe + Frontend + DB) again

--- backend/storage/storage.go
+++ backend/storage/storage.go
@@ -1,7 +1,9 @@
 package storage
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/sharipovr/pflDockerBilling/models"
 
@@ -12,15 +14,17 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	// dsn := fmt.Sprintf(
-	// 	"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-	// 	os.Getenv("DB_HOST"),
-	// 	os.Getenv("DB_USER"),
-	// 	os.Getenv("DB_PASSWORD"),
-	// 	os.Getenv("DB_NAME"),
-	// 	os.Getenv("DB_PORT"),
-	// )
-	dsn := "host=localhost user=rustemsharipov password=postgres dbname=pfl_docker_billing port=5432 sslmode=disable"
+	// Контейнерная версия подключения к БД
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+	// Обычная (не-контейнерная) версия подключения к БД
+	// dsn := "host=localhost user=rustemsharipov password=postgres dbname=pfl_docker_billing port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Ошибка подключения к БД: ", err)
