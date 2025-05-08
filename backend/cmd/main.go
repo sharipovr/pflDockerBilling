614506a587f13c930ef3c0f74c2c4103afcf01d5
Rustem Sharipov <sharipov.rustem@gmail.com>
2025-05-07 20:38:55 -0500

    Removed hardcoded dependency from env.

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,21 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/joho/godotenv"
 	"github.com/sharipovr/pflDockerBilling/routes"
 	"github.com/sharipovr/pflDockerBilling/storage"
 )
 
 func main() {
-	// Загружаем переменные окружения из файла .env
-	if err := godotenv.Load("../.env.local"); err != nil {
-		log.Println("Файл .env.local не найден, используются переменные окружения системы")
-		if err := godotenv.Load(".env.docker"); err != nil {
-			log.Println("Файл .env.docker не найден, используются переменные окружения системы")
-		}
-	}
 	storage.Init()
-
 	router := routes.SetupRouter()
 
 	if err := router.Run(":8080"); err != nil {
